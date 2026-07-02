50eb2b231e9b1f5add2d616a72cae060ad4f13cb
OASI-dev <gamingabcde25@gmail.com>
2026-07-02 15:11:36 +0600

    Solved Week 1 Problem Set - All 5 problems

---
+++ deep.py
@@ -0,0 +1,6 @@
+x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+match x:
+    case "42" | "Forty-Two" | "Forty Two":
+     print("Yes")
+    case _:
+        print("No")
