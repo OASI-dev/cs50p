cfd5004d5cee4eabcb8fc3a20e40c8aada498672
OASI-dev <gamingabcde25@gmail.com>
2026-07-11 17:15:41 +0600

    solved week 7 problems

---
+++ response.py
@@ -0,0 +1,6 @@
+import re
+email = input("What's your email address? ")
+if re.search(r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", email):
+    print("Valid")
+else:
+    print("Invalid")
